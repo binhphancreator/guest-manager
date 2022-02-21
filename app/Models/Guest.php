c175386ffb8ddb5950b2279eddeedf5311388389
binhphancreator <binhpv.B18PT031@stu.ptit.edu.vn>
2022-02-21 09:57:13 +0700

    add global scope

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Guest extends Model
 {
@@ -14,5 +15,12 @@ class Guest extends Model
     public function group()
     {
         return $this->belongsTo('App\Models\Group');
+    }
+
+    protected static function booted()
+    {
+        static::addGlobalScope('active', function (Builder $builder) {
+            $builder->where('is_active', true);
+        });
     }
 }
